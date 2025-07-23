649c84b043e7a2d012bf7f24ed777be5cd16ff53
sadia afzail <F20232661057@umt.edu.pk>
2025-07-23 11:28:54 -0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,24 @@
+
 """
 URL configuration for core project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+"""
+URL configuration for core project.
+
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
@@ -23,7 +40,7 @@ from django.conf.urls.static import static
 web_patterns = [
     path('admin/', admin.site.urls),
     # resume_builder web URLs
-    path('web/', include('resume_builder.web.urls')),
+path('web/', include('resume_builder.web.urls')),
     path('web/', include('accounts.web.urls')),
     path('', include('dashboard.urls')),
 ]
